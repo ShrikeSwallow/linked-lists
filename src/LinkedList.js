908eead827f519ce1263a56e7e1f013f9dd9fb55
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-04 06:56:05 +0000

    Implement append method, head, tail and size properties

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,3 +1,29 @@
+import Node from "./Node";
 export default class LinkedList {
-  constructor() {}
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  append = (value) => {
+    if (this.size === 0) {
+      this.head = new Node(value);
+      this.tail = this.head;
+    } else {
+      this.tail.nextNode = new Node(value);
+      this.tail = this.tail.nextNode;
+    }
+
+    this.size += 1;
+  };
+  prepend = (value) => {};
+  at = (index) => {};
+  pop = () => {};
+  contains = (value) => {};
+  find = (value) => {};
+  toString = () => {};
+
+  // extra credit methods
+  insertAt = (value, index) => {};
+  removeAt = (index) => {};
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./styles.css";
 import LinkedList from "./LinkedList";
 import Node from "./Node";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.head);
+console.log(list.tail);
